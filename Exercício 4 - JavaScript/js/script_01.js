a9043d5209ce5407794002880799abff7f21b0d9
xNeohaha <tuberanthony@gmail.com>
2025-10-24 22:51:47 -0300

    2410/2

--- Exercício 4 - JavaScript/js/script_01.js
+++ Exercício 4 - JavaScript/js/script_01.js
@@ -1,7 +1,7 @@
 const paragrafo = document.getElementById("p_texto");
 const button = document.getElementById("btn_1");
 function MudarTexto() {
-paragrafo.textContent = "Voce gosta de homens kkkkk"
+paragrafo.textContent = "CONCORDO!"
 }
 
 
