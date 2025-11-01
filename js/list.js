11dcc57d97ed6fbcd10ac1e069d5cca7cdbb71a0
xNeohaha <tuberanthony@gmail.com>
2025-11-01 14:01:45 -0300

    0111/05 - Atualização do Portfólio

--- js/list.js
+++ js/list.js
@@ -1,40 +1,61 @@
 window.onload = () => { // Começa quando carrega a janela
   const container = document.querySelector(".product-cards"); //Container onde fica os Cards
-// Codigo feito com muita fé e ajuda do Diego.
+  // Codigo feito com muita fé e ajuda do Diego.
   try {
-    const response = fetch('./data/projetos.json').then( // "Puxa" o JSON 
+    const response = fetch('./data/projetos.json').then( // "Puxa" e chama o JSON 
       response => {
         return response.json();
       }
     ).then(
       projetos => {
         projetos.forEach(projeto => { // Pra cada projeto, repete as ações abaixo com os dados do JSON.
-        console.log("Criado: "+ projeto.titulo) // Alerta que deu certo de certa forma
-        const card = document.createElement("div"); // Cria a DIV
-        card.classList.add("card"); // Cria o Class Card
+          console.log("Criado: " + projeto.titulo) // Alerta que deu certo de certa forma
+          const card = document.createElement("div"); // Cria a DIV
+          card.classList.add("card"); // Cria o Class Card
 
-        const title = document.createElement("h3"); // Cria um H3
-        title.textContent = projeto.titulo; // Seta o texto do H3 para o conteúdo "titulo" do JSON.
-        card.appendChild(title); // NÃO SEI SÓ EXPLICAR MAS PEGA O NODE DA CHILD DE UM PARENTE, sendo a const title.
+          const title = document.createElement("h3"); // Cria um H3
+          title.textContent = projeto.titulo; // Seta o texto do H3 para o conteúdo "titulo" do JSON.
+          card.appendChild(title);
 
-        const desc = document.createElement("p"); // Cria o <p> paragrafo da silva
-        desc.textContent = projeto.descricao; // Puxa a descrição do Json
-        card.appendChild(desc); // PEGA O NODE DA CHILD DE UM PARENTE, sendo a const desc.
+          if (projeto.semestre) { // Verifica se o Há um semestre atribuido.
 
-        projeto.links.forEach(linkData => { // Pega os links e repete caso haja mais de um. (CASO HAJA MAIS DE UM)
-          const link = document.createElement("a"); // Cria o anexo
-          link.href = linkData.url; // Puxa o url do Json
-          link.textContent = linkData.texto; // Puxa o texto do JSON
-          card.appendChild(link); // PEGA O NODE DA CHILD DE UM PARENTE, sendo a const link.
+            const semester = document.createElement("h4"); // Cria um H4
+            semester.textContent = (projeto.semestre + "º Semestre"); // Seta o texto do H4 para o conteúdo "semestre" do JSON.
+            card.appendChild(semester);
+          } else {
+            console.error("O projeto " + projeto.titulo + " não possui semestre.");
+          }
+          if (projeto.status) {
+
+            const statustext = document.createElement("h4"); // Cria um H4
+            if (projeto.status == true) {
+              statustext.textContent = ("O exercício está completo."); // Seta o texto do H4 para o conteúdo "semestre" do JSON.
+            } else if (projeto.status == false) {
+              statustext.textContent = ("O exercício está incompleto. Talvez um erro."); // Seta o texto do H4 para o conteúdo "semestre" do JSON.
+
+            }
+            card.appendChild(statustext);
+          }
+
+          const desc = document.createElement("p"); // Cria o <p> paragrafo da silva
+          desc.textContent = projeto.descricao; // Puxa a descrição do Json
+          card.appendChild(desc); // PEGA O NODE DA CHILD DE UM PARENTE, sendo a const desc.
+
+
+          projeto.links.forEach(linkData => { // Pega os links e repete caso haja mais de um. (CASO HAJA MAIS DE UM)
+            const link = document.createElement("a"); // Cria o anexo
+            link.href = linkData.url; // Puxa o url do Json
+            link.textContent = linkData.texto; // Puxa o texto do JSON
+            card.appendChild(link);
+          });
+
+          container.appendChild(card); // Finaliza o Card
+          console.log("Titulo: " + projeto.titulo + "\nDescrição: " + projeto.descricao) // Recap
         });
-
-        container.appendChild(card); // Finaliza o Card
-         console.log("Titulo: "+ projeto.titulo + "\nDescrição: "+ projeto.descricao) // Recap
-      });
       }
     )
 
- 
+
   } catch (error) {
     console.error("Erro ao carregar os projetos:", error); // Incompetência do Programador 
     container.innerHTML = "<p>Não foi possível carregar os projetos.</p>";
